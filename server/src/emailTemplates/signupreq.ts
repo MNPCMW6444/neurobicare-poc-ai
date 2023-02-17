f92481f639877fba123ffa0cee267bebd257a05d
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-02-17 21:51:37 +0200

    lkn

--- server/src/emailTemplates/signupreq.ts
+++
@@ -1,4 +0,0 @@
-export default (key: number): string =>
-  "<h1>You will need this Key (never give it to anyone):</h1><p>" +
-  key +
-  "</p>";
---
+++ server/src/models/dataModel.ts
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const dataSchema = new mongoose.Schema({
+  data: { type: String, required: true },
+  tag: { type: String, required: true },
+});
+
+export default mongoose.model("data", dataSchema);
