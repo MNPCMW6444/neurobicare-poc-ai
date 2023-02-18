202ea906967393e3dfa8560e3349b36989af99f0
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-02-18 14:38:37 +0200

    Update ResponseTime.tsx

--- src/comps/response/ResponseTime.tsx
+++ src/comps/response/ResponseTime.tsx
@@ -48,6 +48,7 @@ export default function ResponseTime({
         console.log(toSend);
 
         axios.post(domain + "/train", { ...toSend });
+        axios.get(domain + "/areyoualive");
       }
 
       setScores([
