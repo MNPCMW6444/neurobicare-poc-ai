c0d1b727c47768ec3198213521c2a8c0b71e2540
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-02-18 12:05:31 +0200

    awd

---
+++ server/built/index.js
@@ -0,0 +1,58 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const brain_js_1 = __importDefault(require("brain.js"));
+const app = (0, express_1.default)();
+const port = process.env.PORT || 6555;
+dotenv_1.default.config();
+let mainDbStatus = false;
+const connectToDB = () => {
+    mongoose_1.default.set("strictQuery", false);
+    mongoose_1.default.connect("" + process.env.MONGO, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }, (err) => {
+        if (err)
+            return console.error(err);
+        console.log("Connected to Main MongoDB");
+        mainDbStatus = true;
+    });
+    if (!mainDbStatus)
+        setTimeout(connectToDB, 180000);
+};
+connectToDB();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.get("/areyoualive", (_, res) => res.json({ answer: "yes" }));
+const annSchema = new mongoose_1.default.Schema({
+    network: Object,
+});
+const ANN = mongoose_1.default.model("ANN", annSchema);
+app.post("/train", async (req, res) => {
+    const { input, output } = req.body;
+    const lastAnn = await ANN.findOne().sort({ createdAt: -1 }).exec();
+    if (!lastAnn) {
+        const net = new brain_js_1.default.NeuralNetwork({
+            inputSize: 2,
+            outputSize: 1,
+        });
+        net.train([{ input, output }]);
+        const ann = new ANN({
+            network: net.toJSON(),
+        });
+        await ann.save();
+        return res.status(200).send("ANN trained and saved!");
+    }
+    const net = new brain_js_1.default.NeuralNetwork().fromJSON(lastAnn.network);
+    net.train([{ input, output }]);
+    lastAnn.network = net.toJSON();
+    await lastAnn.save();
+    res.status(200).send("ANN updated!");
+});
+app.listen(port, () => console.log(`Server started on port: ${port}`));
