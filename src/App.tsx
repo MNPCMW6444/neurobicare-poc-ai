6bf3a2c6b20447b64882f15bfcff474dee23973f
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-02-20 14:50:34 +0200

    efc

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import Button from "@mui/material/Button";
 import { EEGReading } from "muse-js/dist/lib/muse-interfaces";
 import { useState } from "react";
 import { connectToMuse } from "./muse/connection";
-import { Observable } from "rxjs";
+import { Observable, interval, from } from "rxjs";
+import { map, flatMap } from "rxjs/operators";
 import { addChannelSample } from "./store/reducers/museReducer";
 import { useDispatch } from "react-redux";
 import Grid from "@mui/material/Grid";
@@ -10,11 +11,18 @@ import EEGProvider from "./EEGProvider";
 import NeurobicareColors from "./comps/util/NeurobicareColors";
 import { Typography } from "@mui/material";
 
+import data1 from "./mock/datas.json";
+import data2 from "./mock/datas2.json";
+
 function App() {
   const [eegO, seteegO] = useState<Observable<EEGReading>>();
   const dispatch = useDispatch();
 
   eegO && eegO.subscribe((eeg) => dispatch(addChannelSample(eeg)));
+
+  const mockDataJSON = [...data1, ...data2];
+  let mockData = mockDataJSON.map((doc) => JSON.parse(doc.data));
+  mockData = mockData.map((data) => ({ data: data.input.data }));
 
   return (
     <Grid
@@ -59,7 +67,7 @@ function App() {
                 borderRadius: "35px",
               }}
               variant="contained"
-              onClick={async () => seteegO((await connectToMuse()) as any)}
+              onClick={async () => seteegO(mockMuseEEG())}
             >
               {eegO ? "disconnect" : "connect to virtual muse"}
             </Button>
@@ -71,5 +79,42 @@ function App() {
     </Grid>
   );
 }
+
+const samples = () => {
+  return Array(12)
+    .fill()
+    .map((_) => Math.random())
+    .map(function (x) {
+      return x * 100;
+    });
+};
+
+const customCount = (start: any, end: any, step = 1) => {
+  const len = Math.floor((end - start) / step) + 1;
+  return Array(len)
+    .fill()
+    .map((_, idx) => start + idx * step);
+};
+
+const transform = (index: number) => {
+  const timestamp = Date.now();
+  let chanNums = customCount(0, (window as any).nchans - 1);
+  return from(chanNums).pipe(
+    map((electrode) => ({
+      timestamp,
+      electrode,
+      index,
+      samples: samples(),
+    }))
+  );
+};
+
+const mockMuseEEG = (sampleRate = 256) => {
+  let index = 0;
+  return interval(1000 / sampleRate).pipe(
+    map(() => (index += 1)),
+    flatMap(transform)
+  );
+};
 
 export default App;
